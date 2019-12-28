070cad01dc245128a69e970740d1f303f79180dc
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2019-12-28 13:30:54 -0800

    Updates

---
+++ precache-manifest.e3839f211c6ee8361a2b119b08212c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765c8b712d2e9249aea464d9f1fb592b",
+    "url": "/Redux/index.html"
+  },
+  {
+    "revision": "76515d13812f475cc651",
+    "url": "/Redux/static/css/main.13312f89.chunk.css"
+  },
+  {
+    "revision": "fdc5b2265b332b4ec5dd",
+    "url": "/Redux/static/js/2.7a604e8e.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Redux/static/js/2.7a604e8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76515d13812f475cc651",
+    "url": "/Redux/static/js/main.06952b9a.chunk.js"
+  },
+  {
+    "revision": "390e1c6eefdacf4d9317",
+    "url": "/Redux/static/js/runtime-main.471e23f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
