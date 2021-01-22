0857879356e1971482c5ccc22c96586b77a105b8
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2021-01-21 20:58:11 -0800

    updated baseurl

--- src/API.js
+++ src/API.js
@@ -2,18 +2,27 @@ import axios from "axios";
 
 //const BASEURL = "/api/search/"
 //const BASEURL = "/api/search/"
-const BASEURL = "https://sv443.net/jokeapi/category/";
+const BASEURL = "https://v2.jokeapi.dev/";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   search: async function (query, res) {
-    res = axios.get(BASEURL + query)
-    return res.then(console.log("requested on API page.")).catch(err => { 
-      alert("There is a bug in the Redux State. Try changing the Category in the dropdown!");
-      throw err; 
+    res = axios.get(BASEURL + query);
+    return res.then(console.log("requested on API page.")).catch((err) => {
+      // console.log(query);
+      // console.log(res);
+      alert(
+        "There is a bug in the Redux State. Try changing the Category in the dropdown!"
+      );
+      throw err;
     });
   },
   searchall: function (query) {
-    return axios.get(BASEURL + query).then(({ data }) => data).catch(err => { throw err; });
-  }
+    return axios
+      .get(BASEURL + query)
+      .then(({ data }) => data)
+      .catch((err) => {
+        throw err;
+      });
+  },
 };
--- src/pages/Joke.js
+++ src/pages/Joke.js
@@ -1,94 +1,101 @@
-import React, { Component, useState } from 'react';
-import { Link } from 'react-router-dom';
-import API from '../API';
-import store from '../store/Store';
+import React, { Component, useState } from "react";
+import { Link } from "react-router-dom";
+import API from "../API";
+import store from "../store/Store";
 
 function Joke() {
-    let category = store.getState();
-    console.log(category.Category)
+  let category = store.getState();
+  console.log(category.Category);
 
-    const [dropdownchoice, setdropdownchoice] = useState(category.Category);
-    const [joke, setJoke] = useState("My random joke goes here and will be replaced with redux.");
-    const [delivery, setDelivery] = useState("")
-    const [history, setHistory] = useState(category.History)
+  const [dropdownchoice, setdropdownchoice] = useState(category.Category);
+  const [joke, setJoke] = useState(
+    "My random joke goes here and will be replaced with redux."
+  );
+  const [delivery, setDelivery] = useState("");
+  const [history, setHistory] = useState(category.History);
 
-    const changeCategory = (e) => {
-        setdropdownchoice(e.target.value);
-        let newChoice = e.target.value;
-        console.log(newChoice);
-        if (e.target.value === 'Random') {
-            store.dispatch({ type: 'RANDOM', data: 'Random' });
-        } else if (e.target.value === 'Miscellaneous') {
-            store.dispatch({ type: 'MISC', data: 'Miscellaneous' });
-        } else if (e.target.value === 'Programming') {
-            store.dispatch({ type: 'PROGRAMMING', data: 'Programming' });
-        } else if (e.target.value === 'Dark') {
-            store.dispatch({ type: 'DARK', data: 'Dark' });
-        } else {
-            store.dispatch({ type: 'RANDOM', data: 'Random' });
-        }
-        API.search(e.target.value).then((res) => {
-            console.log(res)
-            let jokeholder = res.data.joke || res.data.setup;
-            setJoke(jokeholder)
-            setDelivery(res.data.delivery || "")
-        })
+  const changeCategory = (e) => {
+    setdropdownchoice(e.target.value);
+    let newChoice = e.target.value;
+    console.log(newChoice);
+    if (e.target.value === "Random") {
+      store.dispatch({ type: "RANDOM", data: "Random" });
+    } else if (e.target.value === "Miscellaneous") {
+      store.dispatch({ type: "MISC", data: "Miscellaneous" });
+    } else if (e.target.value === "Programming") {
+      store.dispatch({ type: "PROGRAMMING", data: "Programming" });
+    } else if (e.target.value === "Dark") {
+      store.dispatch({ type: "DARK", data: "Dark" });
+    } else {
+      store.dispatch({ type: "RANDOM", data: "Random" });
     }
+    API.search(e.target.value).then((res) => {
+      console.log(res);
+      let jokeholder = res.data.joke || res.data.setup;
+      setJoke(jokeholder);
+      setDelivery(res.data.delivery || "");
+    });
+  };
 
-    const newJoke = () => {
-        API.search(category.Category).then((res) => {
-            console.log(res)
-            console.log(res.status)
-            if (res === null || undefined) { return alert("ERRRRORs") };
-            console.log(res)
-            let jokeholder = res.data.joke || res.data.setup;
-            setJoke(jokeholder)
-            setDelivery(res.data.delivery || "")
-            console.log(joke);
-            let addJoke = {}
-            if (res.data.joke) {
-                addJoke.joke = res.data.joke;
-            } else {
-                addJoke.setup = res.data.setup;
-                addJoke.delivery = res.data.delivery
-            }
+  const newJoke = () => {
+    API.search(category.Category).then((res) => {
+      console.log("hello eveerybody");
+      console.log(res);
+      console.log(res.status);
+      if (res === null || undefined) {
+        return alert("ERRRRORs");
+      }
+      console.log(res);
+      let jokeholder = res.data.joke || res.data.setup;
+      setJoke(jokeholder);
+      setDelivery(res.data.delivery || "");
+      console.log(joke);
+      let addJoke = {};
+      if (res.data.joke) {
+        addJoke.joke = res.data.joke;
+      } else {
+        addJoke.setup = res.data.setup;
+        addJoke.delivery = res.data.delivery;
+      }
 
-            setHistory(addJoke);
-        });
-    }
-    return (
-        <>
-            <div className="jumbotron">
-                <Link to='/'><h1 className="jokeheader">Joke Generator</h1></Link>
-                <p className="jokeaterisk">* Limited to 25 req per minute</p>
-            </div>
-            <form action="#" onChange={(e) => changeCategory(e)}>
-                <div class="select-box">
-                    <label for="select-box1" class="label select-box1"><span class="label-desc">{dropdownchoice}</span> </label>
-                    <select id="select-box1" class="select">
-                        <option value="Programming">Programming</option>
-                        <option value="Miscellaneous">Miscellaneous</option>
-                        <option value="Dark">Dark</option>
-                        <option value="Random">Random</option>
-                    </select>
-
-                </div>
-
-            </form>
-            <center>
-                <div className="jokecontent">
-                    <h4 className='theJoke'> {joke}</h4>
-                    <h4 className='theJoke'> {delivery}</h4>
-                    <button onClick={newJoke}>Show me more!</button>
-                </div>
-            </center>
-            {/* <div className="history">
+      setHistory(addJoke);
+    });
+  };
+  return (
+    <>
+      <div className="jumbotron">
+        <Link to="/">
+          <h1 className="jokeheader">Joke Generator</h1>
+        </Link>
+        <p className="jokeaterisk">* Limited to 25 req per minute</p>
+      </div>
+      <form action="#" onChange={(e) => changeCategory(e)}>
+        <div class="select-box">
+          <label for="select-box1" class="label select-box1">
+            <span class="label-desc">{dropdownchoice}</span>{" "}
+          </label>
+          <select id="select-box1" class="select">
+            <option value="Programming">Programming</option>
+            <option value="Miscellaneous">Miscellaneous</option>
+            <option value="Dark">Dark</option>
+            <option value="Random">Random</option>
+          </select>
+        </div>
+      </form>
+      <center>
+        <div className="jokecontent">
+          <h4 className="theJoke"> {joke}</h4>
+          <h4 className="theJoke"> {delivery}</h4>
+          <button onClick={newJoke}>Show me more!</button>
+        </div>
+      </center>
+      {/* <div className="history">
                 history div
                 {history.map(item => <p>{item.joke}</p>)}
                 {aliases.map(item => <li>{item}</li>)} 
 
             </div> */}
-        </>
-    )
+    </>
+  );
 }
-export default Joke
+export default Joke;
